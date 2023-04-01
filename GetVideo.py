1d3fbdebe6455564588f70d789e781282f7b439f
Jam <jamnguyen102@gmail.com>
2023-04-01 12:45:18 -0400

    added angle and stuff

--- GetVideo.py
+++ GetVideo.py
@@ -1,4 +1,5 @@
 import asyncio
+import math
 from threading import Thread
 from djitellopy import Tello
 import cv2
@@ -6,14 +7,35 @@ import time
 from image_proc import *
 
 w, h = 1280, 720
+camera_center = (w/2, h/2)
 centroid = None
 
 
-def telloGetFrame(theDrone, w=360, h=240):
+def telloGetFrame(theDrone, w=w, h=h):
     myFrame = theDrone.get_frame_read()
     myFrame = myFrame.frame
     img = cv2.resize(myFrame, (w, h))
     return img
+
+def approach():
+
+    distance = math.sqrt(math.pow(centroid[0]-camera_center[0], 2) + math.pow(centroid[1]-camera_center[1], 2))
+    print("approaching")
+    # camera center == obj centroid => do smth
+    if distance == 0 or centroid[1] == camera_center[1]:
+        return None
+    
+    # else correct the angle
+    alpha = math.acos((centroid[1]-camera_center[1])/distance)
+    print(f'alpha = {alpha*57.3}')
+
+    turn_right = centroid[0] > camera_center[0]
+    alpha = alpha if alpha < 90 else 180 - alpha
+
+    return turn_right, int(alpha*57.3)
+                           
+
+
 
 def run_robot(drone):
     count = 0
@@ -30,23 +52,33 @@ def run_robot(drone):
 
 def flight_controller(drone:Tello):
     async def main(): #this is a function declared inside of another function
-        try:
+        # try:
             drone.takeoff()
             await asyncio.sleep(0.5)
             print("taken off")
-            while True:
+            while True: # finding centroid
                 drone.rotate_clockwise(20)
-                asyncio.sleep(0.8)
+                await asyncio.sleep(0.5)
                 print("turning")
+
                 if centroid is not None:
                     print(f"found! {centroid}")
-                    drone.land()
-                    time.sleep(2)
-                    break
-        finally:
-            print("stream off")
-            drone.streamoff()
-            drone.end()
+                    while True:
+                        cw, forward = approach()
+                        print(f"centroid = {centroid}")
+
+
+                        if forward == None: #when close enough or something
+                            drone.move_forward(50)
+                        elif cw == True:
+                            drone.rotate_clockwise(forward)
+                        else:
+                            drone.rotate_counter_clockwise(forward)
+                        drone.move_forward(50)
+        # # finally:
+        #     print("stream off")
+        #     drone.streamoff()
+        #     drone.end()
 
     asyncio.run(main())
 
@@ -65,6 +97,7 @@ def main():
     drone = Tello()
     # Connecting the drone to the python script after connecting to the Drone's WiFi
     drone.connect()
+    print(drone.get_battery())
     drone.streamon()
     time.sleep(2)
     fly_thread = Thread(target=flight_controller, daemon=True, kwargs={"drone":drone})
